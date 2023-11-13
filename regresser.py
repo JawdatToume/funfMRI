dd11cf7415164219b2ace7f29de7052cd1f3ed7b
Jawdat <57550053+JawdatToume@users.noreply.github.com>
2023-11-13 14:33:20 -0700

    made into class

--- regresser.py
+++ regresser.py
@@ -20,39 +20,45 @@ You can call regress separately or use the command line
 If you don't input anything it just assumes 4 random 1000 x 1 matrices
 
 returns the linear regressors and the correlation coefficient'''
-
-def main():
-
-    # fmridata = masker.maskSubject(1)
-    # #h5py.File("Subject1_ImageNetTraining.h5", 'r')
-    # #h5py.File("Subject1_ImageNetTest.h5", 'r')
-
-    # x_train = fmridata[:][:][:40]
-    # x_test = fmridata[:][:][40:]
-    # #y_train = h5py.File("Subject1.h5", 'r') 
-    # #y_train = np.array(y_train["dataset"][:])
-    # y_train = torch.load(os.path.join("convnext_train", "01518878_5958.pt"))
-    # A. open folder with pytorch so we can extract the features from the .pt files.
-    # TODO: Figure out how to identify the (technical term) thingies.
-    if(len(sys.argv) > 1):
-        regress(np.random.random(sys.argv[0], sys.argv[1], sys.argv[2], sys.argv[3]))
-    else:
-        regress(np.random.random((1000,1)), np.random.random((1000,1)), np.random.random((1000,1)), np.random.random((1000,1)))
-
-def regress(flattened_data, feature_vector, test_data, result):
-    linregs = []
-    for i in range(len(feature_vector)):
-        linregs.append(SparseLinearRegression(n_iter=100))
-        linregs[i].fit(flattened_data.T, feature_vector[i][:].reshape(1, feature_vector.shape[1]))
+class regressor:
+    def fit(self, flattened_data, feature_vector):
+        self.linregs = []
+        for i in range(len(feature_vector)):
+            self.linregs.append(SparseLinearRegression(n_iter=100))
+            print(feature_vector[i,:].shape)
+            self.linregs[i].fit(flattened_data, feature_vector[:,i])
     
-    predicted_features = []
-    for regressor in linregs:
-        predicted_features.append(regressor.predict(test_data.T)[0])
-    predicted_features = np.array(predicted_features)
-    
-    correlation = np.corrcoef(result.reshape(result.shape[0],), predicted_features)
+    def predict(self, test_data):
+        predicted_features = []
+        for regressor in self.linregs:
+            predicted_features.append(regressor.predict(test_data.T)[0])
+        predicted_features = np.array(predicted_features)
 
-    return linregs, correlation
+        return predicted_features 
 
-if __name__ == "__main__":
-    main()
+    def evaluate(self, predicted_features, result):
+        correlation = np.corrcoef(result.reshape(result.shape[0],), predicted_features)
+
+        return correlation
+
+# def main():
+
+#     # fmridata = masker.maskSubject(1)
+#     # #h5py.File("Subject1_ImageNetTraining.h5", 'r')
+#     # #h5py.File("Subject1_ImageNetTest.h5", 'r')
+
+#     # x_train = fmridata[:][:][:40]
+#     # x_test = fmridata[:][:][40:]
+#     # #y_train = h5py.File("Subject1.h5", 'r') 
+#     # #y_train = np.array(y_train["dataset"][:])
+#     # y_train = torch.load(os.path.join("convnext_train", "01518878_5958.pt"))
+#     # A. open folder with pytorch so we can extract the features from the .pt files.
+#     # TODO: Figure out how to identify the (technical term) thingies.
+#     if(len(sys.argv) > 1):
+#         regress(np.random.random(sys.argv[0], sys.argv[1], sys.argv[2], sys.argv[3]))
+#     else:
+#         regress(np.random.random((1000,1)), np.random.random((1000,5)), np.random.random((1000,1)), np.random.random((1000,5)))
+
+
+# if __name__ == "__main__":
+#     main()
