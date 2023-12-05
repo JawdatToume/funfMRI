c9c573d00ffe6b99362189cd665fd0ba8e763fa1
Jawdat <57550053+JawdatToume@users.noreply.github.com>
2023-12-05 04:57:41 -0700

    Updated Plots

---
+++ graphs.bat
@@ -0,0 +1,17 @@
+.\plotgraphs.py .\Results\results_originalFitting_slir.csv .\Results\results_originalFitting_slir_test2345.csv
+.\plotgraphs.py .\Results\results_originalFitting_slir_test2345.csv .\Results\results_originalFitting_slir_test2345.csv
+.\plotgraphs.py .\Results\results_originalFitting_slir_training2345.csv .\Results\results_originalFitting_slir_test2345.csv
+.\plotgraphs.py .\Results\results_SHIFT_AVG_ridge_predTestAVG5_trainTestAVG1.csv .\Results\results_originalFitting_slir_test2345.csv
+.\plotgraphs.py .\Results\results_SHIFT_AVG_ridge_predTrain1234_trainTestAVG1234.csv .\Results\results_originalFitting_slir_test2345.csv
+.\plotgraphs.py .\Results\results_SHIFT_oops_ridge_pred1_test.csv .\Results\results_originalFitting_slir_test2345.csv
+.\plotgraphs.py .\Results\results_SHIFT_PRENORM_POS_ridge.csv .\Results\results_originalFitting_slir_test2345.csv
+.\plotgraphs.py .\Results\results_SHIFT_PRENORM_POS_ridge_sub1pred2_test.csv .\Results\results_originalFitting_slir_test2345.csv
+.\plotgraphs.py .\Results\results_SHIFT_PRENORM_ridge.csv .\Results\results_originalFitting_slir_test2345.csv
+.\plotgraphs.py .\Results\results_SHIFT_PRENORM_ridge_pred2345_test.csv .\Results\results_originalFitting_slir_test2345.csv
+.\plotgraphs.py .\Results\results_SHIFT_ridge_predTestAVG5_trainTestAVG1234.csv .\Results\results_originalFitting_slir_test2345.csv
+.\plotgraphs.py .\Results\results_SHIFT_ridge_predTrain1_trainTest1.csv .\Results\results_originalFitting_slir_test2345.csv
+.\plotgraphs.py .\Results\results_SHIFT_ridge_predTrain1_trainTestAVG1234.csv .\Results\results_originalFitting_slir_test2345.csv
+.\plotgraphs.py .\Results\results_SHIFT_ridge_predTrain5_trainTest1.csv .\Results\results_originalFitting_slir_test2345.csv
+.\plotgraphs.py .\Results\results_SHIFT_ridge_predTrain5_trainTest12.csv .\Results\results_originalFitting_slir_test2345.csv
+.\plotgraphs.py .\Results\results_SHIFT_ridge_predTrain12_trainTest12.csv .\Results\results_originalFitting_slir_test2345.csv
+.\plotgraphs.py .\Results\results_BASELINE_predTrain1_trainTest1.csv .\Results\results_originalFitting_slir_test2345.csv
